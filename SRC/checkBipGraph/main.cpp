d6b480c1ba54d7abf28ac6464bca625180f9a311
浩 李 <penghao.lee@outlook.com>
2020-12-31 11:29:50 +0800

    增加二分图判断文件

---
+++ SRC/checkBipGraph/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by charol on 2020/12/31.
+//
+
+#include "main.h"
+
+int main(){
+
+    cout << "Hello World !"<<endl;
+    return 0;
+}
---
+++ SRC/checkBipGraph/main.h
@@ -0,0 +1,15 @@
+//
+// Created by charol on 2020/12/31.
+//
+
+#ifndef STRUCTURETASKB_MAIN_H
+#define STRUCTURETASKB_MAIN_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+
+#endif //STRUCTURETASKB_MAIN_H
